ecd85f4d1abd10238b6efabcb83e85f7a394f39a
Ayman Odeh Olmeda <a.odeh.olmeda@gmail.com>
2021-05-30 18:42:45 +0200

    prompt mess

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,36 @@
-const miTitulo = document.querySelector('h1');
-miTitulo.textContent = "Hola mundo!";
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === "images/a330-1.jpg") {
+        miImage.setAttribute('src', "images/b747.jpg");
+    }
+    else {
+        miImage.setAttribute('src', "images/a330-1.jpg");
+    }
+}
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector('h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, introduce tu nombre');
+    if (!miNombre){
+        estableceNombreUsuario()
+    }
+    else{
+        localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es genial, ' + miNombre;
+    }   
+}
+
+if (!localStorage.getItem('nombre')){
+    estableceNombreUsuario();
+}
+else {
+   let nombreAlmacenado = localStorage.getItem('nombre');
+   miTitulo.textContent =  'Mozilla es genial, ' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
